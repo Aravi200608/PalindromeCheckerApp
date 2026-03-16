a28a597b9c8924b7eafb411245a94f99c94c54fb
Aravi200608 <dd1834@srmist.edu.in>
2026-03-16 15:01:14 +0530

    Aravina Fixed project structure

---
+++ palindromecheckerapp.java
@@ -0,0 +1,12 @@
+package PalindromeCheckerApp;
+
+/**
+ * @author Aravina
+ * @version 1.0
+ * Use Case 1: Welcome Page
+ */
+public class palindromecheckerapp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App");
+    }
+}
